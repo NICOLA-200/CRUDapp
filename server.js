3ab2bf0dd06dcaad16b39ea0e03a9a6e1c891641
NICOLA-200 <fatepepe66@gmail.com>
2023-06-20 18:42:49 +0200

    added the morgan

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
+
+const connectDB = require('./server/database/connection');
+
+const app = express();
+
+dotenv.config( { path : 'config.env'} )
+const PORT = process.env.PORT || 8080
+
+// log requests
+app.use(morgan('tiny'));
