9ddcab8440b581ae772971900510db72f1e98297
NICOLA-200 <fatepepe66@gmail.com>
2023-06-28 06:32:18 +0200

    finished the service

--- server/services/render.js
+++ server/services/render.js
@@ -13,3 +13,17 @@ exports.homeRoutes = (req, res) => {
 
     
 }
+
+exports.add_user = (req, res) =>{
+     res.render('add_user');
+ }
+ 
+ exports.update_user = (req, res) =>{
+     axios.get('http://localhost:8080/api/users', { params : { id : req.query.id }})
+         .then(function(userdata){
+             res.render("update_user", { user : userdata.data})
+         })
+         .catch(err =>{
+             res.send(err);
+         })
+ }
