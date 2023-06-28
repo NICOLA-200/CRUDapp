a0ba711f1811d1612bd1a0906c24c3d37fe41b05
NICOLA-200 <fatepepe66@gmail.com>
2023-06-28 06:29:25 +0200

    added theroutes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -21,3 +21,13 @@ route.get('/add-user', services.add_user)
  *  @method GET /update-user
  */
 route.get('/update-user', services.update_user)
+
+
+// API
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
+
+
+module.exports = route
