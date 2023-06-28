93c8148f739f7514208e3ce3dd1c308eadde2013
NICOLA-200 <fatepepe66@gmail.com>
2023-06-28 06:33:05 +0200

    started the controllers

---
+++ server/controller/controller.js
@@ -0,0 +1,17 @@
+var Userdb = require('../model/model');
+
+// create and save new user
+exports.create = (req,res)=>{
+    // validate request
+    if(!req.body){
+        res.status(400).send({ message : "Content can not be emtpy!"});
+        return;
+    }
+
+    // new user
+    const user = new Userdb({
+        name : req.body.name,
+        email : req.body.email,
+        gender: req.body.gender,
+        status : req.body.status
+    })
