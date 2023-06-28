a719df8df2898e468d89d4eec8dd25317a112bfd
NICOLA-200 <fatepepe66@gmail.com>
2023-06-28 06:33:28 +0200

    saving the user working

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -15,3 +15,19 @@ exports.create = (req,res)=>{
         gender: req.body.gender,
         status : req.body.status
     })
+
+    
+    // save user in the database
+    user
+        .save(user)
+        .then(data => {
+            //res.send(data)
+            res.redirect('/add-user');
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message : err.message || "Some error occurred while creating a create operation"
+            });
+        });
+
+}
