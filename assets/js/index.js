64c617b652a3af49ad65a5b1f4e39290f201a163
NICOLA-200 <fatepepe66@gmail.com>
2023-07-11 08:01:56 +0200

    finished the index

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,22 @@ $("#add_user").submit(function(event){
       })
   
   })
+  if(window.location.pathname == "/"){
+     $ondelete = $(".table tbody td a.delete");
+     $ondelete.click(function(){
+         var id = $(this).attr("data-id")
+ 
+         var request = {
+             "url" : `http://localhost:8080/api/users/${id}`,
+             "method" : "DELETE"
+         }
+ 
+         if(confirm("Do you really want to delete this record?")){
+             $.ajax(request).done(function(response){
+                 alert("Data Deleted Successfully!");
+                 location.reload();
+             })
+         }
+ 
+     })
+ }
